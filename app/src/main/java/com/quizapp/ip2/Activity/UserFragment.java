3f9e5566a43ff282d71d03618b9190027fb9a3cf
Aaron <me@aaron.dog>
2018-03-25 21:34:48 +0100

    Updated registration validation Still requries validation for emails

--- app/src/main/java/com/quizapp/ip2/Activity/UserFragment.java
+++ app/src/main/java/com/quizapp/ip2/Activity/UserFragment.java
@@ -33,8 +33,10 @@ public class UserFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.user_fragment, container, false);
         friendsLayout = (LinearLayout) view.findViewById(R.id.friendLinearLayout);
+
         ImageView powerImage = (ImageView) view.findViewById(R.id.imgPowerIcon);
         ImageView settingsImage = (ImageView) view.findViewById(R.id.imgSettingsIcon);
+
         progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
         progressBar.setVisibility(View.VISIBLE);
 
