57ebd8ec23f8db9326e40e969a299ed4ce34eb37
Aaron <me@aaron.dog>
2018-03-30 19:23:43 +0100

    Experimenting with styles

--- app/src/main/java/com/quizapp/ip2/Activity/SettingsActivity.java
+++ app/src/main/java/com/quizapp/ip2/Activity/SettingsActivity.java
@@ -1,19 +1,20 @@
 package com.quizapp.ip2.Activity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import com.quizapp.ip2.R;
-
-import java.util.ArrayList;
 
 /**
  * Created by Allan on 26/03/2018.
@@ -28,6 +29,16 @@ public class SettingsActivity extends Activity {
         setContentView(R.layout.activity_settings);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        final Switch switchLight = (Switch) findViewById(R.id.switchLight);
+        final Switch switchDark = (Switch) findViewById(R.id.switchDark);
+        final Switch switchMono = (Switch) findViewById(R.id.switchMono);
+
+        final TextView txtLight = (TextView) findViewById(R.id.txtLight);
+        final TextView txtDark = (TextView) findViewById(R.id.txtDark);
+        final TextView txtMono = (TextView) findViewById(R.id.txtMono);
+
+        Button btnLogOut = (Button) findViewById(R.id.btnLogOut);
+
 
         Drawable whiteArrow = getDrawable(R.drawable.arrow_back);
         whiteArrow.setTint(getResources().getColor(R.color.colorLight));
@@ -39,6 +50,95 @@ public class SettingsActivity extends Activity {
                 startActivity(intent);
             }
         });
+
+        //Listen for LIGHT SWITCH toggle
+        switchLight.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    switchDark.setChecked(false);
+                    switchDark.setClickable(true);
+                    txtDark.setTypeface(null, Typeface.NORMAL);
+
+                    switchMono.setChecked(false);
+                    switchMono.setClickable(true);
+                    txtMono.setTypeface(null, Typeface.NORMAL);
+
+                    switchLight.setClickable(false);
+                    txtLight.setTypeface(txtLight.getTypeface(), Typeface.BOLD);
+                    //TODO Change theme to light
+                }
+            }
+        });
+
+        //Listen for DARK SWITCH toggle
+        switchDark.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    switchLight.setChecked(false);
+                    switchLight.setClickable(true);
+                    txtLight.setTypeface(null, Typeface.NORMAL);
+
+                    switchMono.setChecked(false);
+                    switchMono.setClickable(true);
+                    txtMono.setTypeface(null, Typeface.NORMAL);
+
+                    switchDark.setClickable(false);
+                    txtDark.setTypeface(txtDark.getTypeface(), Typeface.BOLD);
+                    //TODO Change theme to dark
+                }
+            }
+        });
+
+        //Listen for MONO SWITCH toggle
+        switchMono.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    switchLight.setChecked(false);
+                    switchLight.setClickable(true);
+                    txtLight.setTypeface(null, Typeface.NORMAL);
+
+                    switchDark.setChecked(false);
+                    switchDark.setClickable(true);
+                    txtDark.setTypeface(null, Typeface.NORMAL);
+
+                    switchMono.setClickable(false);
+                    txtMono.setTypeface(txtMono.getTypeface(), Typeface.BOLD);
+                    //TODO Change theme to mono
+                }
+            }
+        });
+
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
+                builder.setTitle("Log out");
+                builder.setMessage("Are you sure you want to log out?");
+
+                builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                builder.setPositiveButton("YES", new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog, int which){
+                        Intent intent = new Intent(SettingsActivity.this, AuthenticationActivity.class);
+                        startActivity(intent);
+                    }
+                });
+                AlertDialog ad = builder.create();
+                ad.show();
+
+
+            }
+        });
+
 
     }
 }
--- app/src/main/java/com/quizapp/ip2/Activity/UserFragment.java
+++ app/src/main/java/com/quizapp/ip2/Activity/UserFragment.java
@@ -1,7 +1,5 @@
 package com.quizapp.ip2.Activity;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -10,7 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
@@ -34,42 +31,10 @@ public class UserFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.user_fragment, container, false);
         friendsLayout = (LinearLayout) view.findViewById(R.id.friendLinearLayout);
-
-        //ImageView powerImage = (ImageView) view.findViewById(R.id.imgPowerIcon);
-        Button btnLogOut = (Button) view.findViewById(R.id.btnLogOut);
-        //ImageView settingsImage = (ImageView) view.findViewById(R.id.imgSettingsIcon);
         Button btnSettings = (Button) view.findViewById(R.id.btnSettings);
 
         progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
         progressBar.setVisibility(View.VISIBLE);
-
-        btnLogOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-                builder.setTitle("Log out");
-                builder.setMessage("Are you sure you want to log out?");
-
-                builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                });
-
-                builder.setPositiveButton("YES", new DialogInterface.OnClickListener(){
-                    @Override
-                    public void onClick(DialogInterface dialog, int which){
-                        Intent intent = new Intent(getContext(), AuthenticationActivity.class);
-                        startActivity(intent);
-                    }
-                });
-                AlertDialog ad = builder.create();
-                ad.show();
-
-
-            }
-        });
 
         btnSettings.setOnClickListener(new View.OnClickListener() {
             @Override
