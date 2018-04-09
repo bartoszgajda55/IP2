dc52f60f53e229e85b256a640a58298dcf7d1916
allancalderwood <ACALDE202@Caledonian.ac.uk>
2018-04-09 17:30:45 +0100

    Added Activites and XML for all admin features Linked each admin fragment to correct activity

---
+++ app/src/main/java/com/quizapp/ip2/Activity/AddQuizActivity.java
@@ -0,0 +1,17 @@
+package com.quizapp.ip2.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.quizapp.ip2.R;
+
+public class AddQuizActivity extends AppCompatActivity {
+
+    //TODO EVERYTHING
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_quiz);
+    }
+}
